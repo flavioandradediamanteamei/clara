a41583119c373b8fb97abc3651182fab3834dabd
flavioandradediamanteamei <flaviodigitalpublicidade@gmail.com>
2025-07-14 02:59:00 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-// Arquivo JavaScript compilado
+document.getElementById("root").innerHTML = `
+  <h1 style="color: purple; font-family: sans-serif;">Oiê! 🌸 Eu sou a Clara</h1>
+  <p style="font-size: 18px;">Seja bem-vindo ao Projeto Diamantes!</p>
+  <button onclick="alert('Você clicou!')">Clique aqui</button>
+`;
